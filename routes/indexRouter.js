df2573c7993b760130d6235f49bfcf26cd4c40ce
Jon Chen <jchen2190@gmail.com>
2023-03-29 01:28:43 -0400

    add current time + temp

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -2,13 +2,14 @@ const express = require("express");
 const router = express.Router();
 const axios = require("axios");
 
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
     try {
-        // let weatherApp = await axios.get("")
+        let weatherApp = await axios.get("https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch")
 
-        // let weatherArray = [];
+        let currentTime = weatherApp.data.hourly.time[0];
+        let currentTemp = weatherApp.data.hourly.temperature_2m[0];
 
-        // res.render("home", {weather: weatherArray})
+        res.render("home", {time: currentTime, temp: currentTemp})
     } catch (error) {
         console.log(error);
     }
