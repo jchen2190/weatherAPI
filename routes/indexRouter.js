eb701151e6c524b55d0788e72d058d7bf636332b
Jon Chen <jchen2190@gmail.com>
2023-03-29 12:00:31 -0400

    git package-lock

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -4,7 +4,9 @@ const axios = require("axios");
 
 router.get("/", async (req, res) => {
     try {
-        let weatherApp = await axios.get("https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch")
+        // let cityLocation = await axios.get(`http://api.openweathermap.org/geo/1.0/direct?q=${city-name},${state-code},${country-code}&limit=5&appid=${API-key}`)
+
+        let weatherApp = await axios.get("https://api.open-meteo.com/v1/forecast?latitude=40.71&longitude=-74.01&hourly=temperature_2m,precipitation_probability&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timeformat=unixtime&timezone=America%2FNew_York")
 
         let currentTime = weatherApp.data.hourly.time[0];
         let currentTemp = weatherApp.data.hourly.temperature_2m[0];
