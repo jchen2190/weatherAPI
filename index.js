547cc6642f7be966c47136470afa1412b8ef4b74
Jon Chen <jchen2190@gmail.com>
2023-03-28 23:30:55 -0400

    init y, update index, add folders

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const logger = require("morgan");
+const path = require("path");
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+app.use(logger("dev"));
+
+const indexRouter = require("./routes/indexRouter");
+app.use("/", indexRouter);
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server is litening on port ${PORT}...`);
+})
