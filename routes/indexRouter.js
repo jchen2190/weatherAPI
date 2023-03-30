8872ff3380d18d3e9354c499f864ebc2043c495d
Jon Chen <jchen2190@gmail.com>
2023-03-29 23:59:32 -0400

    add current time

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -3,15 +3,27 @@ const router = express.Router();
 const axios = require("axios");
 
 router.get("/", async (req, res) => {
+    var date = new Date();
+    var hr = date.getHours();
+    var min = date.getMinutes();
+    var amOrPm = "";
+    if (hr > 13) { hr -= 12 }
+    if (hr < 10) { hr = "0" + hr }
+    if (min < 10) { min = "0" + min }
+    if (hr < 13) {
+        amOrPm = "am"
+    } else {
+        amOrPm = "pm"
+    }
+    var currentTime = `${hr}:${min} ${amOrPm}`;
     try {
         // let cityLocation = await axios.get(`http://api.openweathermap.org/geo/1.0/direct?q=${city-name},${state-code},${country-code}&limit=5&appid=${API-key}`)
 
         let weatherApp = await axios.get("https://api.open-meteo.com/v1/forecast?latitude=40.71&longitude=-74.01&hourly=temperature_2m,precipitation_probability&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timeformat=unixtime&timezone=America%2FNew_York")
-
-        let currentTime = weatherApp.data.hourly.time[0];
+        let weatherForecastHr = weatherApp.data.hourly.time
         let currentTemp = weatherApp.data.hourly.temperature_2m[0];
 
-        res.render("home", {time: currentTime, temp: currentTemp})
+        res.render("home", {time: currentTime, temp: currentTemp, hours: weatherForecastHr})
     } catch (error) {
         console.log(error);
     }
