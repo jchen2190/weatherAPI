9f4926d73fcf7aaecf0d52d9e4861088943a1a0e
Jon Chen <jchen2190@gmail.com>
2023-03-30 22:16:17 -0400

    fixed hr + add temp symbol

--- routes/indexController.js
+++ routes/indexController.js
@@ -1,5 +1,28 @@
 const axios = require("axios");
 
+let latitude = "";
+let longitude = "";
+
+// async function searchCity(req, res) {
+//     let query = req.query.search;
+//     try {
+//         let city = await axios.get(`https://api.api-ninjas.com/v1/city?name=${query}`)
+//         latitude = city.latitude + ""
+//         longitude = city.longitude + ""
+//         console.log(latitude);
+//         console.log(longitude);
+//         res.render("home");
+//         getWeather()
+//     } catch (error) {
+//         let errorObj = {
+//             message: "failure to find city",
+//             payload: error
+//         }
+//         console.log(errorObj);
+//         res.json(errorObj);
+//     }
+// }
+
 async function getWeather(req, res) {
     var date = new Date();
     var hr = date.getHours();
@@ -14,12 +37,17 @@ async function getWeather(req, res) {
     if (hr < 10) { hr = "0" + hr }
     if (min < 10) { min = "0" + min }
     var currentTime = `${hr}:${min} ${amOrPm}`;
+
+    // if(latitude == "" || longitude == "") {
+    latitude = "40.71";
+    longitude = "-74.01";
     try {
-        let weatherApp = await axios.get("https://api.open-meteo.com/v1/forecast?latitude=40.71&longitude=-74.01&hourly=temperature_2m,relativehumidity_2m,dewpoint_2m,apparent_temperature,precipitation_probability,precipitation,cloudcover,visibility&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timeformat=unixtime")
+        let weatherApp = await axios.get(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m,relativehumidity_2m,dewpoint_2m,apparent_temperature,precipitation_probability,precipitation,cloudcover,visibility&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timeformat=unixtime`)
         let weatherForecastHr = weatherApp.data.hourly.time
         let currentTemp = weatherApp.data.hourly.temperature_2m[0];
+        let tempSymbol = weatherApp.data.hourly_units.apparent_temperature;
 
-        res.render("home", {time: currentTime, temp: currentTemp, hours: weatherForecastHr})
+        res.render("home", {time: currentTime, temp: currentTemp, hours: weatherForecastHr, tempSymbol: tempSymbol})
     } catch (error) {
         let errorObj = {
             message: "failure to find weather",
@@ -29,21 +57,6 @@ async function getWeather(req, res) {
         res.json(errorObj);
     }
 }
-
-// async function getCity(req, res) {
-//     try {
-//         let cityApp = await axios.get(`https://api.api-ninjas.com/v1/city?name=${cityName}`)
-//         console.log(cityApp);
-//         getWeather();
-//     } catch (error) {
-//         let errorObj = {
-//             message: "failure to find city",
-//             payload: error
-//         }
-//         console.log(errorObj);
-//         res.json(errorObj);
-//     }
-// }
 
 module.exports = {
     getWeather
