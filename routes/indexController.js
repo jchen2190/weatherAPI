570fb5186d4260cc8b54458b5d626f20f905dc8f
Jon Chen <jchen2190@gmail.com>
2023-04-10 23:48:29 -0400

    add weather daily

--- routes/indexController.js
+++ routes/indexController.js
@@ -32,26 +32,26 @@ async function getWeather(req, res) {
             cityState = city.data.results[0].admin1;
         }
         let weatherApp = await axios.get(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m,relativehumidity_2m,dewpoint_2m,apparent_temperature,precipitation_probability,precipitation,rain,showers,snowfall,cloudcover,cloudcover_low,cloudcover_mid,cloudcover_high,visibility,windspeed_10m&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset,precipitation_hours&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timeformat=unixtime&timezone=America%2FNew_York`)
-        let weatherForecastHr = weatherApp.data.hourly.time
-        let currentTemp = Math.round(weatherApp.data.hourly.temperature_2m[0]);
-        let tempSymbol = weatherApp.data.hourly_units.apparent_temperature;
-        let cloudCover = weatherApp.data.hourly.cloudcover[0];
-        let highTemp = weatherApp.data.daily.temperature_2m_max[0];
-        let lowTemp = weatherApp.data.daily.temperature_2m_min[0];
+        let weatherHr = weatherApp.data.hourly
+        let weatherDaily = weatherApp.data.daily;
+        let currentTemp = Math.round(weatherHr.temperature_2m[0]);
+        let cloudCover = weatherHr.cloudcover[0];
+        let highTemp = weatherDaily.temperature_2m_max[0];
+        let lowTemp = weatherDaily.temperature_2m_min[0];
 
-        let precipitation = weatherApp.data.hourly.precipitation[0];
-        let rain = weatherApp.data.hourly.rain[0];
-        let showers = weatherApp.data.hourly.showers[0];
-        let snowfall = weatherApp.data.hourly.snowfall[0];
+        let precipitation = weatherHr.precipitation[0];
+        let rain = weatherHr.rain[0];
+        let showers = weatherHr.showers[0];
+        let snowfall = weatherHr.snowfall[0];
 
-        let windSpeed = weatherApp.data.hourly.windspeed_10m[0] + " mph";
-        let humidity = weatherApp.data.hourly.relativehumidity_2m[0] + "%";
-        let dewPoint = weatherApp.data.hourly.dewpoint_2m[0] + "°";
-        let visibility = Math.floor(weatherApp.data.hourly.visibility[0] / 5280);
+        let windSpeed = weatherHr.windspeed_10m[0] + " mph";
+        let humidity = weatherHr.relativehumidity_2m[0] + "%";
+        let dewPoint = weatherHr.dewpoint_2m[0] + "°";
+        let visibility = Math.floor(weatherHr.visibility[0] / 5280);
         if (visibility > 10) { visibility = 10 };
         visibility += " mi";
 
-        let sunriseDate = new Date(weatherApp.data.daily.sunrise[0] * 1000);
+        let sunriseDate = new Date(weatherDaily.sunrise[0] * 1000);
         let sunriseHr = sunriseDate.getHours();
         let sunriseMin = sunriseDate.getMinutes();
         let sunriseAmOrPm;
@@ -61,7 +61,7 @@ async function getWeather(req, res) {
         sunrise = sunriseHr < 10 ? "0" + sunriseHr + ":" + sunriseMin : sunriseHr + ":" + sunriseMin;
         if (sunriseHr > 12) { sunriseHr -= 12 };
 
-        let sunsetDate = new Date(weatherApp.data.daily.sunset[0] * 1000);
+        let sunsetDate = new Date(weatherDaily.sunset[0] * 1000);
         let sunsetHr = sunsetDate.getHours();
         let sunsetMin = sunsetDate.getMinutes();
         let sunsetAmOrPm;
@@ -72,23 +72,14 @@ async function getWeather(req, res) {
         if (sunsetHr > 12) { sunsetHr -= 12 };
 
         res.render("home", {
-            time: time,
-            hr: hr,
-            amOrPm: amOrPm,
-            currentTime: currentTime,
-            amOrPm: amOrPm,
+            time: time, hr: hr, currentTime: currentTime, amOrPm: amOrPm,
             temp: currentTemp,
-            weatherForecastHr: weatherForecastHr,
-            tempSymbol: tempSymbol,
+            weatherHr: weatherHr, weatherDaily: weatherDaily,
             cloudCover: cloudCover,
             precipitation: precipitation, rain: rain, showers: showers, snowfall: snowfall,
             cityName: cityName, cityState: cityState,
-            highTemp: highTemp,
-            lowTemp: lowTemp,
-            windSpeed: windSpeed,
-            humidity: humidity,
-            dewPoint: dewPoint,
-            visibility: visibility,
+            highTemp: highTemp, lowTemp: lowTemp,
+            windSpeed: windSpeed, humidity: humidity, dewPoint: dewPoint, visibility: visibility,
             sunriseHr: sunriseHr, sunriseMin: sunriseMin, sunriseAmOrPm: sunriseAmOrPm, sunrise: sunrise, 
             sunsetHr: sunsetHr, sunsetMin: sunsetMin, sunsetAmOrPm: sunsetAmOrPm, sunset: sunset,
         })
